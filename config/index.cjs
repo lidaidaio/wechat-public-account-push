76fbe5562903117d90c359b251aea52751610de5
lidaidaio <115166970+lidaidaio@users.noreply.github.com>
2022-10-06 23:48:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,20 +23,20 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9tom6vtfmyWIJ1wirriy_wTn0aE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'LDjnHmlLuLgwEHroCew6pxlN46QfYYQxGMipq588Hjc',
+      useTemplateId: 'mX4Xt9VEItpXxSDWXXHmS7qFdtWEnoRhyWB4d8p2A0k',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-23',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '晴晴', year: '2002', date: '02-10',
+          //type: '*生日', name: '晴晴', year: '2002', date: '02-10',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
           type: '生日', name: '晴晴', year: '2002', date: '03-23',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2022', date: '09-22',
+          //type: '节日', name: '相识纪念日', year: '2022', date: '09-22',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'LDjnHmlLuLgwEHroCew6pxlN46QfYYQxGMipq588Hjc',
+  CALLBACK_TEMPLATE_ID: 'mX4Xt9VEItpXxSDWXXHmS7qFdtWEnoRhyWB4d8p2A0k',
 
   CALLBACK_USERS: [
     {
