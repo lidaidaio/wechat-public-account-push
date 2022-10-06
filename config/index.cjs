6fac91a44e8a78f8b8e67bf1de235d1a3a1d5c78
lidaidaio <115166970+lidaidaio@users.noreply.github.com>
2022-10-06 23:22:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,9 +23,9 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o9tom6vtfmyWIJ1wirriy_wTn0aE',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	MZO11E5hwG3c7SRiivE3YlUJ5S8b1BCQjBvicgebTlc',
+      useTemplateId: 'LDjnHmlLuLgwEHroCew6pxlN46QfYYQxGMipq588Hjc',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '03-22',
+      horoscopeDate: '03-23',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
@@ -53,7 +53,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'MZO11E5hwG3c7SRiivE3YlUJ5S8b1BCQjBvicgebTlc',
+  CALLBACK_TEMPLATE_ID: 'LDjnHmlLuLgwEHroCew6pxlN46QfYYQxGMipq588Hjc',
 
   CALLBACK_USERS: [
     {
